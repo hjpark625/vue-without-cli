ce2746d2dc98b231d01eb3fa93b98e26213aebc2
hjpark625 <zero950@gmail.com>
2024-04-15 22:11:07 +0900

    feat: lint, prettier settings

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Counter from "@/components/Counter.vue";
+import { ref } from 'vue'
+import Counter from '@/components/Counter.vue'
 
-const message = ref("Hello Vue 3!");
+const message = ref('Hello Vue 3!')
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
-import "@/assets/scss/reset.scss";
+import { createApp } from 'vue'
+import App from '@/App.vue'
+import '@/assets/scss/reset.scss'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
