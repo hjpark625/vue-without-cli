f99a11eb3c0dd82bff716cd1dad52c036122006b
hjpark625 <zero950@gmail.com>
2024-04-15 21:35:05 +0900

    feat: Counter 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>{{ message }}</div>
+  <counter />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import Counter from "@/components/Counter.vue";
 
 const message = ref("Hello Vue 3!");
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="counter-wrapper">
+    <button class="button" type="button" @click="onDecrease">-1</button>
+    <span>{{ count }}</span>
+    <button class="button" type="button" @click="onIncrease">+1</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+
+const onIncrease = () => {
+  count.value++;
+};
+
+const onDecrease = () => {
+  count.value--;
+};
+</script>
+
+<style lang="scss" scoped>
+.counter-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2rem;
+  .button {
+    margin: 0 1rem;
+    border-radius: 4px;
+    width: 3rem;
+    height: 3rem;
+    font-size: 1.5rem;
+    border: 1px solid black;
+  }
+}
+</style>
