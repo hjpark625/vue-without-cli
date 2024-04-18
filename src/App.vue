7a7e00f508ea8ced402b4d383367f7f8cf592a67
hjpark625 <zero950@gmail.com>
2024-04-18 14:29:59 +0900

    feat: vue-router 추가 및 적용

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-  <div>{{ message }}</div>
-  <main-counter />
+  <router-view />
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import MainCounter from '@/components/MainCounter.vue'
-
-const message = ref('Hello Vue 3!')
-</script>
