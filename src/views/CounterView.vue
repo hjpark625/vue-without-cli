a62773341d8ebe9312007866d48cd6ab10bac2e6
hjpark625 <zero950@gmail.com>
2024-04-18 14:30:16 +0900

    feat: 라우터 view파일 추가

---
+++ src/views/CounterView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main-counter />
+</template>
+
+<script lang="ts" setup>
+import MainCounter from '@/components/MainCounter.vue'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const message = ref('Hello Vue3!')
+</script>
