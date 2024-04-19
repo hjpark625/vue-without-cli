98384de19561412881d9670ce63c11bcf7b8a68f
hjpark625 <zero950@gmail.com>
2024-04-19 18:28:24 +0900

    refactor: serve -> express 전환

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+
+const app = express()
+
+const distPath = path.join(__dirname, 'dist')
+const indexHtmlPath = path.join(distPath, 'index.html')
+
+if (!fs.existsSync(distPath) || !fs.existsSync(indexHtmlPath)) {
+  console.error(
+    chalk.red(
+      `The ${chalk.yellow('dist')} folder or ${chalk.yellow('index.html')} doesn't exist.\nPlease run ${chalk.blue('pnpm run build')} first.\n`
+    )
+  )
+  process.exit(1)
+}
+
+app.use(express.static(distPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(distPath, 'public/index.html'))
+})
+
+app.listen(3000, () => {
+  console.log(chalk.green('Server is running on http://localhost:3000'))
+})
